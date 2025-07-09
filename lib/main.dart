ca947858d7f01785fba1725e0e95210534c83f4f
Mohamed <gassma803@gmail.com>
2025-07-09 01:00:43 +0100

    decouverte ThemeData().copyWith pour appliquer des theme generalisé

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: const Color.fromARGB(255, 230, 218, 184)),
       home:Expenses(),
     )
   );
